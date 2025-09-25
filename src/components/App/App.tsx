5b80915de231b58ef6a9527fc20d48afc879a6e0
jres <ljresetl@gmail.com>
2025-09-25 21:47:35 +0200

    23

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,6 @@ import VoteOptions from "../VoteOptions/VoteOptions";
 import type { Votes, VoteType } from "../../types/votes";  
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
-// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App() {
   // Створюємо стан votes
@@ -54,7 +53,7 @@ export default function App() {
                   positiveRate={positiveRate}
               />) : (
         <Notification />)}
-      {/* <SpeedInsights /> */}
+   
       </div>
       
      
